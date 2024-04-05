90c927a3f3d0a25a58cd35ca846ea24199f70a5f
Saschavanvliet <saschavanvliet0802@gmail.com>
2024-04-05 20:36:55 +0200

    yuh

--- server.js
+++ server.js
@@ -50,10 +50,12 @@ app.get('/person/:id', function (request, response) {
 })
 
 // Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 7000)
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+
